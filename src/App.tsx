daa797b0be192c5957641b35f722080dcbb036fa
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2025-05-29 16:58:33 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+function App() {
+  return (
+
+    <div className='min-h-dvh flex justify-center items-center flex-col'>
+      <Button>HELLO World </Button>
+
+    </div>
+  )
+ }
+
+export default App
